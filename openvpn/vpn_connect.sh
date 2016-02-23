cf39fee263e811f57a0ce5b88afdf3e32827f352
neutrak <neutrak@example.com>
2016-02-22 18:39:15 -0600

    made vpn connection accept ovpn argument and updated whitelist for firewall

--- openvpn/vpn_connect.sh
+++ openvpn/vpn_connect.sh
@@ -5,11 +5,23 @@ cp /root/scripts/openvpn/auth.txt /etc/openvpn/
 #change directory to where the script expects the auth file to be
 cd /etc/openvpn/
 
+#by default get the files in /etc/openvpn
+#files="/etc/openvpn/*.ovpn"
+
+#get the two-digit US and CA tcp configurations only
+files="/etc/openvpn/us??.*tcp443.ovpn /etc/openvpn/ca??.*tcp443.ovpn"
+
+#if a particular file was specified, then use that
+#otherwise choose at random
+if [ "$?" -gt 1 ]
+then
+	files="$*"
+fi
+
 #get all the openvpn configurations we have available to us
 ovpn_cnfgs=[]
 n=0
-#for f in /etc/openvpn/*.ovpn
-for f in /etc/openvpn/us??.*tcp443.ovpn /etc/openvpn/ca??.*tcp443.ovpn #get the two-digit US and CA tcp configurations only
+for f in $files
 do
 	#use the stored auth credentials to connect to this server
 	cat "$f" | sed 's/auth-user-pass$/auth-user-pass auth\.txt/g' > "${f}_authfixed"
