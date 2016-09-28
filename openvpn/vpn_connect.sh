4e2315a011ec3001cf0ba23a25a90e6401d9fde0
neutrak <neutrak@example.com>
2016-09-28 19:02:49 -0400

    updated vpn_connect script to be more efficient by only editing auth settings for the server it actually selects, instead of all prior to deciding which server to use

--- openvpn/vpn_connect.sh
+++ openvpn/vpn_connect.sh
@@ -8,36 +8,30 @@ cd /etc/openvpn/
 #by default get the files in /etc/openvpn
 #files="/etc/openvpn/*.ovpn"
 
-#get the two-digit US and CA tcp configurations only
-files="/etc/openvpn/us??.*tcp443.ovpn /etc/openvpn/ca??.*tcp443.ovpn"
+#get the US and CA tcp configurations only
+files=(/etc/openvpn/us*.*tcp443.ovpn /etc/openvpn/ca*.*tcp443.ovpn)
 
-#if a particular file was specified, then use that
-#otherwise choose at random
+#if a particular file or list of files was specified, then use those
 if [ "$?" -gt 1 ]
 then
 	files="$*"
 fi
 
-#get all the openvpn configurations we have available to us
-ovpn_cnfgs=[]
-n=0
-for f in $files
-do
-	#use the stored auth credentials to connect to this server
-	cat "$f" | sed 's/auth-user-pass$/auth-user-pass auth\.txt/g' > "${f}_authfixed"
-	mv "${f}_authfixed" "$f"
-	
-	ovpn_cnfgs[$n]="$f"
-	n=$(($n+1))
-done
+#selected file to actually use for a connection
+#choose at random from our list of possible files
+sel_file="${files[$(($RANDOM%(${#files[@]})))]}"
+
+#update the file we're going to use with automatic authentication
+
+#use the stored auth credentials to connect to this server
+cat "${sel_file}" | sed 's/auth-user-pass$/auth-user-pass auth\.txt/g' > "${sel_file}_authfixed"
+mv "${sel_file}_authfixed" "$sel_file"
 
 #kill any existing openvpn connections
 killall openvpn
 
 #create the openvpn connection, with stored login credentials
-#using a random ovpn configuration file
-#cat /root/scripts/openvpn/auth.txt | openvpn ${ovpn_cnfgs[$(($RANDOM%$n))]} &
-openvpn ${ovpn_cnfgs[$(($RANDOM%$n))]} &
+openvpn "${sel_file}" &
 
 #route ALL traffic through the new tun0 interface
 iptables -t nat -A POSTROUTING -s 0.0.0.0/0 -o tun0 -j MASQUERADE
